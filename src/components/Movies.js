cd7fd195881f1879073849bdec07cfae0fe13da7
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-02-17 16:18:33 +0300

    complete assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
-}
+  return (
+    <div>
+    <h1>Movies Page</h1>
+      {movies.map((item)=>{
+        return(
+          
+          <div key={item.title}>
+            <h2>{item.title}</h2>
+            <p>{item.time}</p>
 
+            <p>Genres: </p>
+            <ul>
+            {item.genres.map((item) => <li key={item}>{item}</li>)}
+            </ul>
+            </div>
+            );
+          })}
+
+          </div>
+      
+      );
+    }
 export default Movies;
